939423aa517ec27e50362d835dfb0c2aa33a4a06
venky-98 <sun_venkat2000@yahoo.com>
2025-03-16 21:07:00 +0530

    Update arthmetic operation

--- D1.py
+++ D1.py
@@ -1,8 +1,20 @@
 print("Hello, World!")
+print("*************ArithmeticOperation - Start ****************************")
 a = 5
 b = 3
 sum = a + b
-print("Sum:", sum)
+print("Sum          =",a," + ",b," = ", sum)
+diff = a-b
+print("Differernce  =",a," - ",b," = ", diff)
+mul =a*b
+print("Multiply     =",a," * ",b," = ", mul)
+div = a/b
+print("Division     =",a," / ",b," = ", div)
+floor = a//b
+print("Floor_div    =",a,"// ",b," = ", floor)
+reminder = a%b
+print("Reminder_mod =",a," % ",b," = ", reminder)
+print("*********************************************************************")
 
 num = 4
 if num % 2 == 0:
