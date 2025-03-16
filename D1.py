b54b4a9d46c439f69654a4040bfa268b0d674b98
venky-98 <sun_venkat2000@yahoo.com>
2025-03-16 15:48:53 +0530

    d1 file from local

---
+++ D1.py
@@ -0,0 +1,18 @@
+print("Hello, World!")
+a = 5
+b = 3
+sum = a + b
+print("Sum:", sum)
+
+num = 4
+if num % 2 == 0:
+    print(num, "is Even")
+else:
+    print(num, "is Odd")
+
+numbers = [5, 2, 9, 1, 5, 6]
+
+# Sorting the list in ascending order
+numbers.sort()
+
+print("Sorted list:", numbers)   
