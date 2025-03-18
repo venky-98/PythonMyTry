452e567996c5ee153ca1355da8d96f540db1bafb
venky-98 <sun_venkat2000@yahoo.com>
2025-03-18 08:05:25 +0530

    Update arthmestring-1 operation

--- D1.py
+++ D1.py
@@ -14,6 +14,39 @@ floor = a//b
 print("Floor_div    =",a,"// ",b," = ", floor)
 reminder = a%b
 print("Reminder_mod =",a," % ",b," = ", reminder)
+cap = a^b
+print("Cap          =",a," ^ ",b," = ", cap)
+print("*********************************************************************")
+
+print("*************************STRING**************************************")
+Fname= "Manasa"
+print(Fname)
+print("Length of the string ",Fname," ==> ",len(Fname))
+print("Indexing  of the string [0]",Fname," ==> ",Fname[0])
+print("Indexing  of the string [1]",Fname," ==> ",Fname[1])
+print("Indexing  of the string [2]",Fname," ==> ",Fname[2])
+print("Indexing  of the string [3]",Fname," ==> ",Fname[3])
+print("Indexing  of the string [4]",Fname," ==> ",Fname[4])
+print("Indexing  of the string [5]",Fname," ==> ",Fname[5])
+print("Indexing  of the string [-1]",Fname," ==> ",Fname[-1])
+print("Indexing  of the string [-2]",Fname," ==> ",Fname[-2])
+print("Indexing  of the string [-3]",Fname," ==> ",Fname[-3])
+print("Indexing  of the string [-4]",Fname," ==> ",Fname[-4])
+print("Indexing  of the string [-5]",Fname," ==> ",Fname[-5])
+print("Indexing  of the string [-6]",Fname," ==> ",Fname[-6])
+print("Slice  - [Start: ]of the string [1:]",Fname," ==> ",Fname[1:])
+print("Slice  - [Start: ]of the string [2:]",Fname," ==> ",Fname[2:])
+print("Slice  - [end: ]of the string [:1]",Fname," ==> ",Fname[:1])
+print("Slice  - [end: ]of the string [:2]",Fname," ==> ",Fname[:2])
+print("Slice  - [Start: ]of the string [1:]",Fname," ==> ",Fname[1:])
+print("Slice  - [Start: ]of the string [2:]",Fname," ==> ",Fname[2:])
+print("Slice  - [end: ]of the string [:1]",Fname," ==> ",Fname[:1])
+print("Slice  - [Start:end:step] of the string [0:5:2]",Fname," ==> ",Fname[0:5:2])
+print("Slice  - [Start:end:step] of the string [1:5:2]",Fname," ==> ",Fname[1:5:2])
+print("Slice  - [Start:end:step] of the string [::]",Fname," ==> ",Fname[::])
+print("Slice  - [Start:end:step] of the string [::-1]",Fname," ==> ",Fname[::-1])
+print("Slice  - [Start:end:step] of the string [::-2]",Fname," ==> ",Fname[::-2])
+
 print("*********************************************************************")
 
 num = 4
